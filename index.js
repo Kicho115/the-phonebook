fc334a59c42af73e3c696de33ce7213443f3f70a
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-01-19 05:24:08 -0800

    Exercise 3.3

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get("/api/persons", (req, res) => {
     res.json(persons)
 })
 
+app.get("/api/persons/:id", (req, res) => {
+    const id = req.params.id
+    const person = persons.find((p) => p.id === id)
+    person ? res.json(person) : res.status(404).end()
+})
+
 app.get ("/info", (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>`)
