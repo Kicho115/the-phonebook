9247833ef87fb4b2d9c1da2d76e002713ee6d2fb
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-02-09 03:17:20 -0800

    Add the frontend

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('dist'))
+
 const morgan = require('morgan')
 morgan.token('obj', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :obj'))
